9b903b85d905bba03512e59c43956bd0bd54c286
LucasLeal0619 <lucaseduleal@gmail.com>
2026-04-08 09:41:17 -0300

    Atividade06

---
+++ Atividade06.php
@@ -0,0 +1,18 @@
+<?php 
+
+$nome_da_variavel = 7; 
+
+echo "Tabuada do $nome_da_variavel: <br>"; 
+echo "$nome_da_variavel x 1 = " . ($nome_da_variavel * 1) . "<br>";
+echo "$nome_da_variavel x 2 = " . ($nome_da_variavel * 2) . "<br>";
+echo "$nome_da_variavel x 3 = " . ($nome_da_variavel * 3) . "<br>";
+echo "$nome_da_variavel x 4 = " . ($nome_da_variavel * 4) . "<br>";
+echo "$nome_da_variavel x 5 = " . ($nome_da_variavel * 5) . "<br>";
+echo "$nome_da_variavel x 6 = " . ($nome_da_variavel * 6) . "<br>";
+echo "$nome_da_variavel x 7 = " . ($nome_da_variavel * 7) . "<br>";
+echo "$nome_da_variavel x 8 = " . ($nome_da_variavel * 8) . "<br>";
+echo "$nome_da_variavel x 9 = " . ($nome_da_variavel * 9) . "<br>";
+echo "$nome_da_variavel x 10 = " . ($nome_da_variavel * 10) . "<br>";
+
+
+?>
